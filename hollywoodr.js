7b6d3b76b10c03b75a7d9c2df4bde65de528c6cf
Magnus von Wachenfeldt <magnus@deckhand.se>
2012-01-30 00:36:25 +0100

    debug

--- hollywoodr.js
+++ hollywoodr.js
@@ -147,33 +147,22 @@ function showVideo(videoId) {
 }
 
 (function() {
-    var loadedjQuery = false,
-        loadedSwfobject = false;
-
-    function checkDependencies() {
-        if (loadedSwfobject && loadedjQuery) {
-            $(init);
-        }
-    }
-
     if (typeof(jQuery) == undefined || typeof(jQuery) != "function" || jQuery("*") === null) {
         var jq = document.createElement("script");
         jq.src = "//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js";
         jq.onload = function() {
             $ = jQuery.noConflict(true);
-            loadedjQuery = true;
-            checkDependencies();
         }
         document.body.appendChild(jq);
     }
     if (typeof(swfobject) == "undefined") {
         var swfobj = document.createElement("script");
         swfobj.src = "//ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js";
-        swfobj.onload = function() {
-            loadedSwfobject = true;
-            checkDependencies();
-        }
+
         document.body.appendChild(swfobj);
     }
+
+    $(init);
+
 })()
 })(window, document);
